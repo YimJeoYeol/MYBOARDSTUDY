27d4d0fe945037b0b162c2642c831ca547175e05
IMJE <imjy2@naver.com>
2023-05-10 17:25:31 +0900

    20230510 commit

--- src/test/java/boardexample/myboard/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/boardexample/myboard/domain/member/repository/MemberRepositoryTest.java
@@ -1,11 +1,15 @@
 package boardexample.myboard.domain.member.repository;
 
+import boardexample.myboard.domain.member.Member;
+import boardexample.myboard.domain.member.Role;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -20,6 +24,18 @@ class MemberRepositoryTest {
     @AfterEach
     private void after(){
         em.clear();
+    }
+
+    public void 회원저장_성공() throws Exception{
+        Member member = Member.builder().username("username").password("1234").nickName("Nick").role(Role.USER).age(22).build();
+
+        Member saveMember = memberRepository.save(member);
+
+        Member findeMember = memberRepository.findById(saveMember.getId()).orElseThrow(()-> new RuntimeException("저장된 회원이 없습니다."));     //아직 예외 클래스를 만들지 않았기에 RuntimeException으로 처리하겠습니다.
+
+
+        assertThat(findeMember).isSameAs(saveMember);
+        assertThat(findeMember).isSameAs(member);
     }
 
 }
